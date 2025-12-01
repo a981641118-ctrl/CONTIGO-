f58de42e44f1dae105da156a9256a5a050f4cda2
a981641118-ctrl <a981641118@gmail.com>
2025-11-30 20:29:01 -0500

    Update db.js

--- back-med/db.js
+++ back-med/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+// En la nube usaremos DATABASE_URL. En local, usaremos tus credenciales fijas.
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:root@localhost:5432/medicamentos';
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'medicamentos',
-  password: 'root', 
-  port: 5432,
+  connectionString: connectionString,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
